df45c0d31b7bb4eeabf51129960ef103960ef740
Esttiwar <jestiwar.correa@gmail.com>
2021-09-28 23:02:40 -0500

    reestructuración del login autenticacion con google

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -1,17 +1,18 @@
 import React from 'react';
 import "./Login.css" 
 
+
 const Login = () => {
     return (
         <>
         <div class="signinform">
-        <h1>Login</h1>
+        <h1>Inicia sesión</h1> 
         {/* container */}
         <div class="container">
             {/* main content */}
             <div class="w3l-form-info">
                 <div class="w3_info">
-                    <h2 id="login">Login</h2>
+                    <h2 id="login">Inicia sesión</h2>
                     <form action="#" method="post">
                         <div class="input-group">
                             <span><i class="fas fa-user" aria-hidden="true"></i></span>
@@ -32,28 +33,31 @@ const Login = () => {
                         --> */}
                         <button class="btn btn-primary btn-block" type="submit">Login</button>
                     </form>
-                    {/* <!--
-                        <p class="continue"><span>or Login with</span></p>
+                    
+                        <p class="continue"><span>O</span></p>
                         <div class="social-login">
-                            <a href="#facebook">
+                            {/* <a href="#facebook">
                                 <div class="facebook">
                                     <span class="fab fa-facebook-f" aria-hidden="true"></span>
-                                    
+                    
                                 </div>
                             </a>
                             <a href="#twitter">
                                 <div class="twitter">
                                     <span class="fab fa-twitter" aria-hidden="true"></span>
                                 </div>
-                            </a>
+                            </a> */}
                             <a href="#google">
-                                <div class="google">
-                                    <span class="fab fa-google" aria-hidden="true"></span>
-                                </div>
+                                <button className="login-google"> 
+                                    <span>Continúa con</span> 
+                                    <span><img className="img-google" src="https://img.pngio.com/google-logo-2015-png-image-purepng-free-transparent-cc0-png-google-logo-transparent-10000_3382.png" alt="" /></span>
+                                    {/* <span class="fab fa-google google" aria-hidden="true"></span> */}
+                                
+                                </button>
                             </a>
                         </div>
-                        <p class="account">Don't have an account? <a href="#signup">Sign up</a></p>
-                    --> */}
+                        <p class="account">¿No tienes cuenta? <a href="#signup">Regístrate</a></p>
+                    
                 </div>
             </div>
             {/* <!-- //main content --> */}
