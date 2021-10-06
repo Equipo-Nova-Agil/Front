28218a5b5a9111e4c9e9c95fa4fbbb7b4bc661f0
Esttiwar <jestiwar.correa@gmail.com>
2021-10-06 17:14:45 -0500

    revisando rama

--- src/App.jsx
+++ src/App.jsx
@@ -8,8 +8,8 @@ function App() {
   return (
     <>
     {/* <Login></Login> */}
-    <Ventas></Ventas>
-    {/* <NuevaVenta></NuevaVenta> */}
+    {/* <Ventas></Ventas> */}
+    <NuevaVenta></NuevaVenta>
     </>
   );
 }
