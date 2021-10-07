47d30de0d07fe2fac7204cbaa00a07389cf5c089
Esttiwar <jestiwar.correa@gmail.com>
2021-10-06 19:43:07 -0500

    agregando editar venta

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,15 @@ import './App.css';
 import Login from "./Components/Login"
 import Ventas from './Components/Ventas';
 import NuevaVenta from './Components/NuevaVenta';
+import EditarVenta from './Components/EditarVenta';
 
 function App() {
   return (
     <>
     {/* <Login></Login> */}
     {/* <Ventas></Ventas> */}
-    <NuevaVenta></NuevaVenta>
+    {/* <NuevaVenta></NuevaVenta> */}
+    <EditarVenta></EditarVenta>
     </>
   );
 }
